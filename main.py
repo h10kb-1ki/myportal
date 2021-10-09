44aa71fdd57353ce03210f7780dab1541a2795ff
h10kb-1ki <87002069+h10kb-1ki@users.noreply.github.com>
2021-10-09 16:10:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import re
 traffic = st.checkbox('Traffic')
 if traffic == True:
     '''
-    ### 東海道本線[豊橋～米原]
+    #### 東海道本線[豊橋～米原]
     '''
     url = 'https://transit.yahoo.co.jp/traininfo/detail/192/193/'
     res = requests.get(url)
@@ -26,12 +26,12 @@ if traffic == True:
     st.write(kosinJ)
     st.write(infoJ)
     '''
-    #### JRホームページ
+    ##### ▶JR運行情報
     '''
     st.markdown('https://traininfo.jr-central.co.jp/zairaisen/status_detail.html?line=10001&lang=ja', unsafe_allow_html=True)
     st.write('')
     '''
-    ### 名鉄名古屋本線
+    #### 名鉄名古屋本線
     '''
     url = 'https://transit.yahoo.co.jp/traininfo/detail/208/0/'
     res = requests.get(url)
@@ -44,12 +44,12 @@ if traffic == True:
 
 
     '''
-    #### 名鉄バス
+    ##### ▶名鉄バス
     '''
     st.markdown('https://navi.meitetsu-bus.co.jp/mb/DepQR.aspx?p=320103000', unsafe_allow_html=True)
 
     '''
-    #### 乗り換え案内
+    ##### ▶乗り換え案内
     '''
     st.markdown('https://www.jorudan.co.jp/norikae/', unsafe_allow_html=True)
 
